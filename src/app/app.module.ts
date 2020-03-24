462f17cf61e75994448429e1ef6afaa6fc2ef564
jasonheo <heo9453@hanmail.net>
2020-03-24 20:33:49 +0900

    add dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// PrimeNG
 import { InputTextModule} from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { TableModule  } from 'primeng/table';
 import { DialogModule } from 'primeng/dialog';
+import { DropdownModule} from 'primeng/dropdown';
+
+
+// Component
 import { ListComponent } from './list/list.component';
 
 @NgModule({
@@ -16,11 +24,14 @@ import { ListComponent } from './list/list.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     InputTextModule,
     DialogModule,
     ButtonModule,
-    TableModule
+    TableModule,
+    DropdownModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { SelectItem } from 'primeng/api';
 
+interface City{
+  name: string;
+  code: string;
+}
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -7,9 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  cities1: SelectItem[];
+
+  selectedCity1: City;
+
+  constructor(  ) {
+
+    this.cities1 = [
+      {label:'Select City', value:null},
+      {label:'New York', value:{id:1, name: 'New York', code: 'NY'}},
+      {label:'Rome', value:{id:2, name: 'Rome', code: 'RM'}},
+      {label:'London', value:{id:3, name: 'London', code: 'LDN'}},
+      {label:'Istanbul', value:{id:4, name: 'Istanbul', code: 'IST'}},
+      {label:'Paris', value:{id:5, name: 'Paris', code: 'PRS'}}
+  ];
+  }
 
   ngOnInit(): void {
+
   }
 
 }
