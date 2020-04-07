bd13b11b025f5da4aabc74f7e30d1cf2e091a645
jasonheo <heo9453@hanmail.net>
2020-04-07 19:14:08 +0900

    clear

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,3 @@
-import { CarServiceService } from "./../car-service.service";
 import { Component, OnInit, NgModule } from "@angular/core";
 import { SelectItem } from "primeng/api";
 
@@ -7,39 +6,19 @@ interface City {
   code: string;
 }
 
-export interface Car {
-  vin;
-  year;
-  brand;
-  color;
-}
+// export interface Car {
+//   vin;
+//   year;
+//   brand;
+//   color;
+// }
 @Component({
   selector: "app-list",
   templateUrl: "./list.component.html",
-  styleUrls: ["./list.component.css"]
+  styleUrls: ["./list.component.css"],
 })
 export class ListComponent implements OnInit {
-  cities1: SelectItem[];
+  constructor() {}
 
-  selectedCity1: City;
-
-  cars: Car[];
-
-  constructor(private carService: CarServiceService) {
-    this.cities1 = [
-      { label: "Select City", value: null },
-      { label: "New York", value: { id: 1, name: "New York", code: "NY" } },
-      { label: "Rome", value: { id: 2, name: "Rome", code: "RM" } },
-      { label: "London", value: { id: 3, name: "London", code: "LDN" } },
-      { label: "Istanbul", value: { id: 4, name: "Istanbul", code: "IST" } },
-      { label: "Paris", value: { id: 5, name: "Paris", code: "PRS" } }
-    ];
-  }
-
-  ngOnInit(): void {
-    this.carService.getCarsSmall().then(data => {
-      // window.console.log(data);
-      this.cars = data;
-    });
-  }
+  ngOnInit(): void {}
 }
