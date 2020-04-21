9c45abc6a07863c90ff6d05ac122a288596ee890
your_name <your_email@example.com>
2020-04-21 20:52:06 +0900

    add dropdown

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,8 +1,13 @@
+import { SelectItem } from "primeng/api";
 import { Component, OnInit } from "@angular/core";
 
 import { InputTextModule } from "primeng/inputtext";
 import { DropdownModule } from "primeng/dropdown";
 
+interface Sex {
+  sexName: string;
+  code: string;
+}
 @Component({
   selector: "app-search",
   templateUrl: "./search.component.html",
@@ -11,15 +16,26 @@ import { DropdownModule } from "primeng/dropdown";
 export class SearchComponent implements OnInit {
   //组件主数据
   name;
+  gender: Sex;
 
   //组件非主数据
-  namePlaceholder = "请输入名字";
+  namePlaceholder = '请输入名字';
 
-  constructor() {}
+  genderList: Sex[];
+
+  constructor() {
+    this.genderList = [
+      { sexName: '男', code: 'M' },
+      { sexName: '女', code: 'F' },
+    ];
+
+    this.gender = this.genderList[0];
+  }
 
   ngOnInit(): void {}
 
   onBlur() {
-    alert("The  value : " + this.name);
+    alert("The  value : " + this.name+'  '+this.gender.sexName);
+
   }
 }
